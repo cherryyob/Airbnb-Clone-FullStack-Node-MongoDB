d0444a5e0687adb7346f0d33a7e1bafab12d4705
radhey <cherryyob@gmail.com>
2026-05-10 09:16:10 +0530

    new Version of stay zen

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
-    pageTitle: "Add Home",
-    activePage: "add-Home",
+    pageTitle: "login",
+    activePage: "login",
     isLoggedIn: req.session.isLoggedIn,
 
     editing: false,
@@ -15,4 +15,22 @@ exports.postLogout = (req, res) => {
   req.session.destroy(() => {
     res.status(200).redirect("/login");
   });
+};
+exports.getsingup = (req, res) => {
+  res.status(200).render("auth/singup", {
+    pageTitle: "SingUp",
+    activePage: "singup",
+    isLoggedIn: req.session.isLoggedIn,
+  });
+};
+exports.forgatePassword = (req, res) => {
+  res.status(200).render("auth/forgatePassword", {
+    pageTitle: "Forgate Password",
+    activePage: "login",
+    isLoggedIn: req.session.isLoggedIn,
+  });
+};
+exports.postsingup = (req, res) => {
+  console.log(req.body);
+  res.status(200);
 };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,4 +5,7 @@ const authController = require("../controller/authController");
 authRauter.get("/login", authController.getLogin);
 authRauter.post("/login", authController.postLogin);
 authRauter.post("/logout", authController.postLogout);
+authRauter.get("/singup", authController.getsingup);
+authRauter.get("/forgatePassword", authController.forgatePassword);
+authRauter.post("/singup", authController.postsingup);
 module.exports = authRauter;
